6cdb2061508fcdb60974472fd04839e5383d8cc9
Jennifer Glauche <webmaster@joaz.de>
2015-04-23 19:13:46 +0200

    next try with reinforced structure

--- surgical_tools/lib/printed/pointed_forceps.rb
+++ surgical_tools/lib/printed/pointed_forceps.rb
@@ -1,9 +1,20 @@
 class PointedForceps < Forceps
+	def initialize(args={})
+		# Side wall thickness of the grips
+		@thickness = args[:thickness] || 3.5
+		
+		# This is the height, this should be set according to the rest of the part 
+		@height = args[:height] || 9
+	end
 
 
 	def part(show)
 		res = side
 		res += side.mirror(y:1).translate(y:-13)
+		
+		# making the rear stronger
+		res += cube([13,5,@height]).translate(x:100,y:-9,z:-@height/2.0)
+		
 
 		res -= cube([40,40,20]).rotate(y:-15).translate(x:-0.1,y:-20,z:-3).color("red")
 
